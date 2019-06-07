08348d9466c8943f0bf663b90cfe87ac9f065ace
Karol Nowacki <karol.nowacki_xl@wp.pl>
2019-06-07 20:33:57 +0200

    Pierwszy program Hello. Tablica Args

--- 01_Hello/Program.cs
+++ 01_Hello/Program.cs
@@ -10,6 +10,16 @@ namespace _01_Hello
     {
         static void Main(string[] args)
         {
+            String name = args[0];
+
+            Console.Beep();
+            Console.WriteLine("Hello world");
+            Console.WriteLine(name);
+
+            //foreach (var item in args)
+            //{
+            //    Console.WriteLine("Witaj " +item);
+            //}
         }
     }
 }
